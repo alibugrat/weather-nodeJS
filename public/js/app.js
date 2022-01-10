6ce360d2592c0be87f6bb372ff0b8575729c3d69
Ali Buğra Topçuoğlu <alibugrat@gmail.com>
2022-01-10 21:44:28 +0300

    change fetch url

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,6 @@
 console.log("Client side  javascript file is loaded!")
 
 
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
@@ -14,7 +13,7 @@ weatherForm.addEventListener('submit', (e) => {
 
     const location = search.value
 
-    fetch('http://localhost:3000/weather?address='+location).then((response) =>{
+    fetch('/weather?address='+location).then((response) =>{
         response.json().then((data) =>{
             if(data.error){
                 messageOne.textContent = data.error
